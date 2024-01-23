15a247eaaf238305803d3aa932451a5ce4a7e447
Rafael Santos <rdss.snf20@uea.edu.br>
2024-01-23 12:25:04 -0400

    update the game

--- jogo-inedito/ball.py
+++ jogo-inedito/ball.py
@@ -1,29 +1,11 @@
 class Ball:
 
-    def __init__(self, status, dest_x, dest_y, color, width, height, speedx, speedy):
+    def __init__(self, status, x, y, color, width, height, speedx, speedy):
         self.status = status
-        self.dest_x = dest_x
-        self.dest_y = dest_y
+        self.x = x
+        self.y = y
         self.color = color
         self.width = width
         self.height = height
         self.speedx = speedx
-        self.speedy = speedy
-
-    def set_positionx(self, dest_x):
-        self.dest_x = dest_x
-
-    def set_positiony(self, dest_y):
-        self.dest_y = dest_y
-
-    def get_positionx(self):
-        return self.dest_x
-
-    def get_positiony(self):
-        return self.dest_y
-
-    def set_speedx(self, speedx):
-        self.speedx = speedx
-
-    def set_speedy(self, speedy):
         self.speedy = speedy
--- jogo-inedito/main.py
+++ jogo-inedito/main.py
@@ -1,13 +1,13 @@
 import pygame
-import paddle
-import ball
+import random
 
 pygame.init()
 
 clock = pygame.time.Clock()
+r = random.randint(-5, 5)
 paddle_color = (250, 250, 250)
-screen_width = 1600 #original size 900
-screen_height = 800 #original size 600
+screen_width = 1600  # original size 900
+screen_height = 800  # original size 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('futpong')
 
@@ -15,48 +15,84 @@ pygame.display.set_caption('futpong')
 imp = pygame.image.load("./assets/jogo-campo.jpg").convert()
 field = pygame.transform.scale(imp, (screen_width, screen_height))
 
-player1_quarterback = paddle.Paddle(screen_width/2 -340,  350, paddle_color, 10, 90)
-player1_goalkeeper = paddle.Paddle(100,  385, paddle_color, 10, 45)
-ia_front_paddle_rect = pygame.Rect(screen_width/2 + 340, 350, 10, 90) #original position (60, 250, 10, 90)
-ia_back_paddle_rect = pygame.Rect(screen_width - 120, 385, 10, 45) #original position (810, 275, 10, 45)
+player_front_paddle_rect = pygame.Rect(screen_width / 2 - 340, 350, 10, 90)  # original position (340, 250, 10, 90)
+player_back_paddle_rect = pygame.Rect(100, 385, 10, 45)  # original position (90, 275, 10, 45)
+ia_front_paddle_rect = pygame.Rect(screen_width / 2 + 340, 350, 10, 90)  # original position (60, 250, 10, 90)
+ia_back_paddle_rect = pygame.Rect(screen_width - 100, 385, 10, 45)  # original position (810, 275, 10, 45)
 
-'''
-player_front_paddle_rect = pygame.Rect(screen_width/2 - 340, 350, 10, 90) #original position (340, 250, 10, 90)
-player_back_paddle_rect = pygame.Rect(100, 385, 10, 45) #original position (90, 275, 10, 45)
-ia_front_paddle_rect = pygame.Rect(screen_width/2 + 340, 350, 10, 90) #original position (60, 250, 10, 90)
-ia_back_paddle_rect = pygame.Rect(screen_width - 120, 385, 10, 45) #original position (810, 275, 10, 45)
-'''
-player_front_paddle_rect = pygame.Rect(player1_quarterback.dest_x, player1_quarterback.dest_y, player1_quarterback.width, player1_quarterback.height) #original position (340, 250, 10, 90)
-player_back_paddle_rect = pygame.Rect(player1_goalkeeper.dest_x, player1_goalkeeper.dest_y, player1_goalkeeper.width, player1_goalkeeper.height)
-b = ball.Ball(1 ,screen_width/2 - 8, screen_height/2 - 12, (155, 0, 0), 25, 25, 10, 0)
-ball_rect = pygame.Rect(b.dest_x, b.dest_y, b.width, b.height)
-def ball_left():
-    ball_rect.move_ip(-b.speedx, b.speedy)
-    #return True
+ball_rect = pygame.Rect(screen_width / 2 - 8, screen_height / 2 - 12, 25, 25)
 
-def ball_right():
-    ball_rect.move_ip(b.speedx, b.speedy)
-    #return True
+goal_player_rect1 = pygame.Rect(0, 267, 100, 11)
+goal_player_rect2 = pygame.Rect(0, 528, 100, 11)
+goal_ia_rect1 = pygame.Rect(screen_width - 94, 267, 100, 11)
+goal_ia_rect2 = pygame.Rect(screen_width - 94, 528, 100, 11)
+player_goal_line_rect = pygame.Rect(70, 278, 11, 250)
+ia_goal_line_rect = pygame.Rect(screen_width - 70, 278, 11, 250)
 
+ball_speedx = 7
+ball_speedy = r
+paddle_speed = 0
+max_speed = 12
+hit = False
 status = True
-while status:
+timer = 0
+player_goals = 0
+ia_goals = 0
+
+def balls_moviments():
+    global ball_speedy, ball_speedx, hit
+
+    ball_rect.x = ball_rect.x + ball_speedx
+    ball_rect.y = ball_rect.y + ball_speedy
+
+    if (ball_rect.left <= 0 or ball_rect.right >= screen_width) and hit == False:
+        ball_speedx = -ball_speedx
+        hit = True
+
+    if (ball_rect.top <= 0 or ball_rect.bottom >= screen_height) and hit == False:
+        ball_speedy = -ball_speedy
+        hit = True
+
+    if (ball_rect.x >= 20 and ball_rect.x <= 1400) or (ball_rect.y >= 20 and ball_rect.y <= 1700):
+        hit = False
+
+    if hit == False and (ball_rect.colliderect(player_front_paddle_rect)):
+        ball_speedx = -ball_speedx
+        ball_speedy = -ball_speedy
+        hit = True
+
+
+
+while status :
+    # drawing on screen
     screen.fill((0, 0, 0))
     screen.blit(field, (0, 0))
     player_front_paddle = pygame.draw.rect(screen, paddle_color, player_front_paddle_rect)
-    #player_back_paddle = pygame.draw.rect(screen, paddle_color, player_back_paddle_rect)
+    player_back_paddle = pygame.draw.rect(screen, paddle_color, player_back_paddle_rect)
 
     ia_front_paddle = pygame.draw.rect(screen, paddle_color, ia_front_paddle_rect)
     ia_back_paddle = pygame.draw.rect(screen, paddle_color, ia_back_paddle_rect)
 
-    ball = pygame.draw.rect(screen,(155, 0, 0), ball_rect)
-    if b.status == 1:
-        ball_left()
+    goal_player1 = pygame.draw.rect(screen, (0, 255, 0), goal_player_rect1)
+    goal_player2 = pygame.draw.rect(screen, (0, 255, 0), goal_player_rect2)
+    goal_ia1 = pygame.draw.rect(screen, (0, 255, 255,), goal_ia_rect1)
+    goal_ia2 = pygame.draw.rect(screen, (0, 255, 255,), goal_ia_rect2)
+    player_goal_line = pygame.draw.rect(screen,(255, 0, 255), player_goal_line_rect)
+    ia_goal_line = pygame.draw.rect(screen,(255, 0, 255), ia_goal_line_rect)
+
+    ball = pygame.draw.rect(screen, (155, 0, 0), ball_rect)
+
+    # paddle`s moviments
 
     key = pygame.key.get_pressed()
     if key[pygame.K_w]:
         player_front_paddle_rect.move_ip(0, -10)
+        if player_front_paddle_rect.top <= 0:
+            player_front_paddle_rect.top = 0
     elif key[pygame.K_s]:
         player_front_paddle_rect.move_ip(0, 10)
+        if player_front_paddle_rect.bottom >= screen_height:
+            player_front_paddle_rect.bottom = screen_height
     if key[pygame.K_q]:
         player_back_paddle_rect.move_ip(0, -7)
     elif key[pygame.K_a]:
@@ -70,20 +106,18 @@ while status:
     elif key[pygame.K_k]:
         ia_back_paddle_rect.move_ip(0, 7)
 
-    if pygame.Rect.colliderect(ball, player_front_paddle_rect) and b.status == 1:
-        b.status = 2
-        b.speedy = 0
-        b.speedy = player1_quarterback.dest_y/100
-    if b.status == 2:
-        ball_right()
-    if pygame.Rect.colliderect(ball, ia_front_paddle_rect) and b.status == 2:
-        b.status = 1
-    #player1_quarterback.set_positiony()
-    print(player_front_paddle_rect.update(player1_quarterback.dest_x, player1_quarterback.dest_y, player1_quarterback.width, player1_quarterback.height))
+    balls_moviments()
+    # player_front_paddle_rect.y = player_front_paddle.y + paddle_speed
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             status = False
-    pygame.display.flip()
+        if event.type == pygame.KEYDOWN:
+            if pygame.K_p:
+                status = False
+            if pygame.K_SPACE:
+                status = True
+
+    pygame.display.update()
     clock.tick(60)
 pygame.quit()
